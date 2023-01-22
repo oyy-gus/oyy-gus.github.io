5134d539a3238f9b0b1830ae27caf3f71d8a1b2f
GussJTM <97151655+oyy-gus@users.noreply.github.com>
2023-01-22 14:15:18 +0000

    update

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,6 @@
 VanillaTilt.init(document.querySelectorAll(".link ul li"), {
-    max: 10,
-    speed: 10,
-    glare: true,
-    "max-glare": 1
+  max: 10,
+  speed: 10,
+  glare: true,
+  "max-glare": 1,
 });
